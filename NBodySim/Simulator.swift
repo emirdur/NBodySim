d13480dd77e8404dde388d036323e417bf58b4d6
emirdur <emirdurakovicofficial@gmail.com>
2025-04-26 14:29:35 -0400

    Gravitational Attraction Between Two Particles

--- NBodySim/Simulator.swift
+++ NBodySim/Simulator.swift
@@ -10,7 +10,7 @@ import simd
 
 class Simulator: ObservableObject {
     @Published var particles: [Particle] = []
-    let gravity: Float = 9.8
+    let gravitationalConstant: Float = 0.001
     let dt: Float = 0.016 // 60 fps => 1/60s = 0.016s
     var timer: Timer?
     
@@ -47,8 +47,30 @@ class Simulator: ObservableObject {
     
     func updateParticles() {
         for i in 0..<particles.count {
+            if particles[i].position.x <= 0 || particles[i].position.x >= 500 {
+                particles[i].velocity.x *= -1
+                particles[i].position.x = min(max(particles[i].position.x, 0), 500)
+            }
+            
+            if particles[i].position.y <= 0 || particles[i].position.y >= 500 {
+                particles[i].velocity.y *= -1
+                particles[i].position.y = min(max(particles[i].position.y, 0), 500)
+            }
+            
+            var totalForce = SIMD2<Float>(0, 0)
+            
+            for j in 0..<particles.count where j != i {
+                let direction = particles[i].position - particles[j].position
+                let distanceSquared = max(length_squared(direction), 0.01)
+                let forceMagnitude = gravitationalConstant * particles[i].mass * particles[j].mass / distanceSquared
+                totalForce += forceMagnitude * normalize(direction)
+            }
+            
+            // a = F / m
+            let acceleration = totalForce / particles[i].mass
+            
             // v = v + at
-            particles[i].velocity.y += gravity * dt
+            particles[i].velocity += acceleration * dt
             // x = x + vt
             particles[i].position += particles[i].velocity * dt
         }
